9333face89721f47abdf3e644bd1b85f68857e24
takumi-s-prog <takumi.s-lovelovelive@docomo.ne.jp>
2025-08-08 17:41:40 +0900

    エラー直し

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
      @messages = @room.messages.includes(:user)
   end
-end
+
 
   def create
     @room = Room.find(params[:room_id])
@@ -22,3 +22,4 @@ end
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
+end
